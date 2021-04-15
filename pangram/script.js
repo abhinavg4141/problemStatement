e218e01b2ae97fae292db3a168a73ca71d448928
Abhinav Gupta <abhinavg4141@gmail.com>
2021-04-16 00:02:27 +0530

    printstar

--- pangram/script.js
+++ pangram/script.js
@@ -25,7 +25,7 @@ function update(){
   var sentence = $('#pangram').val();
   var missing = findMissingLetters(sentence);
   var used = findUsedLetters(sentence);
-  var missingMsg = (missing.length == 0) ? "Success!!!" : "Missing: " + missing.join(', ');
+  var missingMsg = (missing.length == 0) ? "Success!" : "Missing: " + missing.join(', ');
   var usedMsg = used.length + " letter" + ((used.length == 1) ? "" : "s");
   $('#result').html(
     '<span class="used">' + usedMsg + '</span><span class="missing">' + missingMsg + '</span>'
--- star/script.js
+++ star/script.js
@@ -1,69 +1,79 @@
 let btn = document.querySelector(".btn");
 let input = document.querySelector(".array");
+let textArea = document.querySelector("#numberline")
 
-btn.addEventListener("click",function(){
-    if(input.value){
+btn.addEventListener("click", function () {
+    if (input.value) {
         let arr = input.value.split(",");
         let starPattern = printStar(arr);
-        console.log(starPattern);
-        var ans = document.createElement("h1");
-        ans.classList.add("ans");
-        ans.innerText = starPattern;
-        document.body.appendChild(ans);
-        alert(starPattern);
+        textArea.value = starPattern;
     }
 })
 
-function printStar(arr){
+function printStar(arr) {
     let max = -100000;
     let min = 100000;
-    for(let i=0;i<arr.length;i++) arr[i] = parseInt(arr[i]);
+    for (let i = 0; i < arr.length; i++) arr[i] = parseInt(arr[i]);
 
-    for(let i=0;i<arr.length;i++){
-        if(arr[i]>max) max = arr[i];
-        if(arr[i]<min) min = arr[i];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) max = arr[i];
+        if (arr[i] < min) min = arr[i];
     }
+    arr.sort(function(a,b){return a-b});
     let newArr = [];
-    if(min>=0) min =0;
+    if (min >= 0) min = 0;
 
 
-    for(let i=min;i<=max;i++) newArr[i + (-min)] = 0;
-    for(let i=0;i<arr.length;i++) newArr[arr[i]+(-min)] = arr[i];
-    
+    for (let i = min; i <= max; i++) newArr[i + (-min)] = 0;
+    for (let i = 0; i < arr.length; i++) newArr[arr[i] + (-min)] = arr[i];
+
+
     console.log(newArr);
 
-    newArr.sort(function(a, b){return a-b});
-    let str=""
+    
+
+    let str = ""
     let loop1 = max;
-    for(let i=0;i<loop1;i++){
-      for(let j=0;j<newArr.length;j++){  
-          if(newArr[j]>=max) str+="* ";
-          else if(newArr[j]<0)str+="   ";
-          else str+="  ";
-      }
-      max--;
-      str+="\n";
+    for (let i = 0; i < loop1; i++) {
+        for (let j = 0; j < newArr.length; j++) {
+            if (newArr[j] >= max) str += " *";
+            else str += "  ";
+        }
+        console.log(str);
+        max--;
+        
+        console.log(max);
+        str+="\n";
     }
 
-    for(let i=min;i<=loop1;i++){
-        if(i>=0) str+=i+" ";
-        else str+=i+""
+    for (let i = min; i <= loop1; i++) {
+        if(i >= 0) str+=" "+i;
+        else str+=i+"";
     }
 
-    str+="\n";
-
+    str += "\n";
     let loop2 = -min;
-      for(let i=0;i<loop2;i++){
-      for(let j=0;j<newArr.length;j++){
-          if(newArr[j]>=min && newArr[j]<0){
-              str+="*  ";
-              newArr[j]++;
-          }
-          else str+="  "
-      }
-          min--;
-          str+="\n"
-      }
-      console.log(str);
-     return str
-  }
+    console.log(newArr);
+    for (let i = 0; i < loop2; i++) {
+        for (let j = 0; j < newArr.length; j++) {
+            if (newArr[j] >= min && newArr[j] < 0) {
+                
+                console.log("inside if");
+                str += " *";
+                newArr[j]++;
+            }
+            else{
+                console.log("inside else");
+                str+="  ";
+            }
+        }
+        min--;
+
+        str += "\n";
+    }
+    console.log(str);
+    return str
+
+}
+
+
